a2aa62c47ceb5ddb489baf210f66e4f803cf2d87
Marc D Wagner <marc.wagner@yahoo.com>
2022-10-25 15:15:05 -0700

    fixed errors, site displays, refactored, added getmapdata

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from '.axios';
+import axios from 'axios';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
@@ -19,44 +19,70 @@ class App extends React.Component {
       lon: '',
     }
   }
+
+  // city input
+  handleCityInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    })
+    console.log(e.target.value);
+  }
+
+  // async data from site
+  getCityData = async (e) => {
+    e.preventDefault();
+    let response;
+    try {
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      // console.log(url);
+      response = await axios.get(url);
+      let location = response.data[0];
+
+      this.setState({
+        cityData: location,
+        error: false,
+        lat: location.lat,
+        lon: location.lon,
+      }, () => {
+        this.getMapData();
+      });
+      
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: error.message
+      })
+    }  
+  }
+  getMapData = async () => {
+    let mapUrl = `https://us1.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.lat},${this.state.lon}&zoom=12`
+
+    let findMap = await axios.get(mapUrl);
+    const map = findMap.config.url;
+
+    this.setState({
+      mapData: map
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Header
+
+        />
+        <CityInput
+          handleCityInput={this.handleCityInput}
+        />
+        <Main
+          cityData={this.state.cityData}
+        />
+        <Footer
+
+        />
+      </>
+    );
+  }
 }
-
-// city input
-handleCityInput = (e) => {
-  e.preventDefault();
-  this.setState({
-    city: e.target.value
-  })
-}
-// asyn data from site
-getCityData = async (e) => {
-  e.preventDefault();
-  console.log(this.state.city);
-  let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
-  let cityData = await axios.get(url);
-}
-
-getMapData = async () +> {
-
-}
-
-render() {
-  return (
-    <>
-    <Header
-
-    />
-    <CityInput
-      handleCityInput={this.handleCityInput}
-    />
-    <Main
-
-    />
-    <Footer
-
-    />
-    </>
-  );
-}
-
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import CityInput from './CityInput';
+// import CityInput from './CityInput';
 
 class Main extends React.Component {
   render() {
-    let chosenCity = 
-    return <CityInput
-
+    return
   }
 };
 
